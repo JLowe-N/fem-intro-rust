bf482561d94e73397483671106c8c6a333b48650
Justin Lowen <justin.lowen@l7informatics.com>
2022-01-01 23:00:58 -0600

    Switch to borrowing arguments

--- part5/src/main.rs
+++ part5/src/main.rs
@@ -1,37 +1,37 @@
 fn main() {
     let numbers = vec![2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24];
 
-    let (sum_of_nums, numbers) = sum(numbers);
-    let (product_of_nums, numbers) = product(numbers);
-    let (average_of_nums, _) = average(numbers);
+    let sum_of_nums = sum(&numbers);
+    let product_of_nums = product(&numbers);
+    let average_of_nums = average(&numbers);
 
     println!("Sum of these numbers: {}", sum_of_nums);
     println!("Product of these numbers: {}", product_of_nums);
     println!("Average of these numbers: {}", average_of_nums);
 }
 
-fn sum(numbers: Vec<i64>) -> (i64, Vec<i64>) {
+fn sum(numbers: &Vec<i64>) -> i64 {
     let mut total = 0;
 
     for num in numbers.iter() {
         total += num;
     }
 
-    (total, numbers)
+    total
 }
 
-fn product(numbers: Vec<i64>) -> (i64, Vec<i64>) {
+fn product(numbers: &Vec<i64>) -> i64 {
     let mut total = 1;
 
     for num in numbers.iter() {
         total *= num;
     }
 
-    (total, numbers)
+    total
 }
 
-fn average(numbers: Vec<i64>) -> (i64, Vec<i64>) {
+fn average(numbers: &Vec<i64>) -> i64 {
     let length = numbers.len() as i64;
 
-    (sum(numbers.clone()).0 / length, numbers)
+    sum(numbers) / length
 }
